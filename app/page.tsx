461658288472363868a62859c2308c406e57031e
AnzorV <anzorv@gmail.com>
2026-03-12 17:56:45 +0000

    implement navigation

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <p className="text-3xl text-indigo-500">Coinamiring</p>
+        <p className="text-3xl text-indigo-500">AmiringPulse</p>
     )
 }
 export default Page
